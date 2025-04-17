5ac2e286628791f69d404cde6bb782a3dd790984
Ilias Hamel <iliashamel041@gmail.com>
2025-04-17 21:43:27 +0200

    correction des bugs

--- src/collision.c
+++ src/collision.c
@@ -1,8 +1,5 @@
 #include "collision.h" // Chemin relatif simplifié, car ils sont dans le même dossier
-<<<<<<< HEAD
 #include <SDL2/SDL.h>
-=======
->>>>>>> 171483704c1faaa65c40ff41fa76f969020e9cd2
 
 bool checkCollision(SDL_Rect a, SDL_Rect b) {
     return SDL_HasIntersection(&a, &b);
@@ -11,7 +8,6 @@ bool checkCollision(SDL_Rect a, SDL_Rect b) {
 void handleCollisions(SDL_Rect* playerRect, float* velocityY, bool* isOnGround, bool* jumping,
                       SDL_Rect ground, SDL_Rect* platforms, int numPlatforms) {
     *isOnGround = false;
-<<<<<<< HEAD
     *velocityY += 0.5f; // Applique une gravité
     playerRect->y += (int)(*velocityY); // Mise à jour de la position Y du joueur en fonction de la vitesse
 
@@ -38,34 +34,6 @@ void handleCollisions(SDL_Rect* playerRect, float* velocityY, bool* isOnGround, 
             else if (*velocityY < 0 && playerRect->y >= p.y + p.h - 10) {
                 playerRect->y = p.y + p.h; // Le joueur est positionné juste en dessous de la plateforme
                 *velocityY = 1; // Le joueur rebondit légèrement
-=======
-    *velocityY += 0.5f;
-    playerRect->y += (int)(*velocityY);
-
-    // Sol
-    if (checkCollision(*playerRect, ground)) {
-        playerRect->y = ground.y - playerRect->h;
-        *velocityY = 0;
-        *isOnGround = true;
-        *jumping = false;
-    }
-
-    // Plateformes
-    for (int i = 0; i < numPlatforms; i++) {
-        SDL_Rect p = platforms[i];
-        if (checkCollision(*playerRect, p)) {
-            // Par-dessus
-            if (*velocityY >= 0 && playerRect->y + playerRect->h <= p.y + 10) {
-                playerRect->y = p.y - playerRect->h;
-                *velocityY = 0;
-                *isOnGround = true;
-                *jumping = false;
-            }
-            // Par-dessous
-            else if (*velocityY < 0 && playerRect->y >= p.y + p.h - 10) {
-                playerRect->y = p.y + p.h;
-                *velocityY = 1;
->>>>>>> 171483704c1faaa65c40ff41fa76f969020e9cd2
             }
         }
     }
--- src/collision.h
+++ src/collision.h
@@ -3,16 +3,8 @@
 
 #include <stdbool.h>
 #include <SDL2/SDL.h>
-#include <stdbool.h>
 
-<<<<<<< HEAD
 void handleCollisions(SDL_Rect *playerRect, float *velocityY, bool *isOnGround, bool *jumping, SDL_Rect ground, SDL_Rect *platforms, int numPlatforms);
-=======
-bool checkCollision(SDL_Rect a, SDL_Rect b);
-
-void handleCollisions(SDL_Rect* playerRect, float* velocityY, bool* isOnGround, bool* jumping,
-                      SDL_Rect ground, SDL_Rect* platforms, int numPlatforms);
->>>>>>> 171483704c1faaa65c40ff41fa76f969020e9cd2
 
 bool checkCollision(SDL_Rect a, SDL_Rect b);
 
